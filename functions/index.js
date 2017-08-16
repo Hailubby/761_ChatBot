307d6cd5f3bcae18921efffc434086c23b443019
Hailubby <hwan561@aucklanduni.ac.nz>
2017-08-16 12:49:01 +1200

    Add logging of chat messages Pairprogrammed with Jay

--- functions/index.js
+++ functions/index.js
@@ -12,12 +12,49 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.webhook = functions.https.onRequest((req, res) => {
-  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'spgetti-token') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
+  if (req.method === 'GET') {
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'spgetti-token') {
+      console.log("Validating webhook");
+      res.status(200).send(req.query['hub.challenge']);
+    } else {
+      console.error("Failed validation. Make sure the validation tokens match.");
+      res.sendStatus(403);
+    }
+  }
+
+  if (req.method === 'POST'){
+    var data = req.body;
+
+      // Make sure this is a page subscription
+      if (data.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function(entry) {
+          var pageID = entry.id;
+          var timeOfEvent = entry.time;
+
+          // Iterate over each messaging event
+          entry.messaging.forEach(function(event) {
+            if (event.message) {
+              receivedMessage(event);
+            } else {
+              console.log("Webhook received unknown event: ", event);
+            }
+          });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
+      }
   }
 });
+
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
 
