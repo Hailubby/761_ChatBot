9e6f1e3b5445d06703b7f41e5ff610c154ddc924
William Lin <wlin888@gmail.com>
2017-08-30 13:23:39 +1200

    Modified message.js to setup dynamic generic messages
    Peer programmed with Ben

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,8 @@ const client = new FBClient();
 client.onMessage(message => {
   if (message.text === 'ping') {
     message.sendMessage('pong');
+  } else if (message.text === 'generic') {
+    message.sendGenericMessage(['Title', 'Subtitle', [['Button Title 1', 'http://www.google.com'], ['Button Title 2', 'http://www.youtube.com']]]);
   } else {
     message.sendMessage(message.text);
   }
