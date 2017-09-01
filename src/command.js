7b0fda7aa6c32643f500d1c805661778041891c5
Akram Darwazeh <a2.darwazeh@gmail.com>
2017-09-01 14:49:20 +1200

    PR Dev/Conversation into master (#7)
    * Make the command object handle regex as a key instead of strings
    
    Pear programmed with:
    - Henry Wu
    - Jay Gradon
    
    * Add command handling
    
    Pair programmed with:  
     - Jay Gradon
     - Henry Wu
    
    * Add eslint dependency
    
    * Fix PR issues.
    
    * Merge master into Dev/conversation
    
    Also do minor refactoring on some classes and add a test follow up.
    
    Pear programmed with:
    - Henry Wu
    - James Mount
    
    * Comment code

--- src/command.js
+++ src/command.js
@@ -2,11 +2,13 @@ class Command {
   /**
    * Command objects encapsulate a conversation between the bot and a single user.
    *
+   * The respond object "function" is responsible for handling multiple responses for the same key.
+   *
    * @param {RegExp} key
    * @param {function} respond
    * @param {Command[]} followup
    */
-  constructor(key="", respond=(msg) => {}, followup = []){
+  constructor(key, respond=msg => {}, followup = []){
     // name of key
     this.key = key;
     // Response this convo gives
