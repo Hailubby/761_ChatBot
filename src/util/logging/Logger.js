37950c8b5ffb1731ab264a75240be8f058a3cd53
Akram <akrdar@gmail.com>
2017-09-28 10:38:42 +1300

    Fix PR issues

--- src/util/logging/Logger.js
+++ src/util/logging/Logger.js
@@ -4,9 +4,9 @@ const GLogger = require('./GoogleSheetsLogger');
  * General purpose logger.
  */
 class Logger {
-    constructor() {
-        this.logger = new GLogger();
-    }
+  constructor() {
+    this.logger = new GLogger();
+  }
 
   /**
    * Writes a log.
@@ -15,21 +15,22 @@ class Logger {
    * @param {string} text The text sent
    * @param {string} label Label of the log (eg. send, receive)
    */
-    log(senderId, text, sendRec) {
-        return this.logger.log(senderId, text, sendRec);
-    }
+  log(senderId, text, sendRec) {
+    return this.logger.log(senderId, text, sendRec);
+  }
 
-    /**
-     * Make the log for a user.
-     *
-     * @param {string} senderId
-     * @param {string} senderName
-     */
-    init(senderId, senderName){
-      return this.logger.makeSheet(senderId, senderName).then(sheetId => {
+  /**
+   * Make the log for a user.
+   *
+   * @param {string} senderId
+   * @param {string} senderName
+   */
+  init(senderId, senderName) {
+    return this.logger.makeSheet(senderId, senderName)
+      .then(sheetId => {
         this.logger.addToToc(senderId, senderName, sheetId);
       });
-    }
+  }
 }
 
 module.exports = Logger;
